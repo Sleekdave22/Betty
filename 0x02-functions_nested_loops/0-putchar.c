197a7db0c82280b67ae0f491b981d5d17a3f1f30
Sleekdave22 <sleekdavinchi@yahoo.com>
2023-09-12 20:43:06 -0700

    j

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /**
 *main - function prints text as output
 *
-*Return:return 0
+*Return: Always 0
 */
 
 int main(void) 
@@ -17,5 +17,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-    return 0;
+return (0);
 }
