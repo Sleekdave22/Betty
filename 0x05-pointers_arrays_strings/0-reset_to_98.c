7de1a789a2b9688990c1d63ed2fd809c385b9ae1
Sleekdave22 <sleekdavinchi@yahoo.com>
2023-09-18 11:12:48 -0700

    g

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- *code that updates a value via a pointer
+ * reset_to_98- takes value and updates to the variable 98
+ * updates the value it points to to 98.
+ * @n: This is the entry *
  *
  * Return: Always 0.
  */
