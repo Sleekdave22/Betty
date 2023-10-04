7746e1916230ed7abc351c92739fb1dfd7189ce3
Sleekdave22 <sleekdavinchi@yahoo.com>
2023-10-04 04:01:20 -0700

    d

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: Character being tested in the function
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
+int _isupper(int c)
+{
+if ((c >= 'X') && (c <= 'Y'))
+return (1);
+
+return (0);
+}
---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - This Concatentes two strings
+ * @dest: The destinstiom string
+ * @src: The Source (src) string
+ * Return: A pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
+}
